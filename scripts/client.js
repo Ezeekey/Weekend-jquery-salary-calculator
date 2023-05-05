0f07071dfeae2a85ad0870fb1d4e883649ec8801
Zeke Taves <ezekielprof@gmail.com>
2023-05-05 16:24:49 -0500

    Implemented basic hiring and firing.

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,29 @@
 $(function () {
     console.log('Screw all yall');
-})
+    
+    // Add listener for delete buttons.
+    $("#tableBody").on("click", ".fire", deleteEmployee);
+
+    // Add listener to add employee.
+    $("#submitButton").on("click", addEmployee);
+});
+
+
+function deleteEmployee(event) {
+    // Delete item off list.
+    $(event.target).closest("tr").remove();
+}
+
+
+function addEmployee() {
+    // Add employee to table.
+    $("#tableBody").append(`<tr>
+    <td>${$("#firstNameInput").val()}</td>
+    <td>${$("#lastNameInput").val()}</td>
+    <td>${$("#idInput").val()}</td>
+    <td>${$("#titleInput").val()}</td>
+    <td>${$("#salaryInput").val()}</td>
+    <td><button class="fire">Fire</button></td>
+    </tr>`);
+    console.log("WWOOOOORK");
+}
